7ca2e85ef5b2d69387dad6f928d11af6fae88d4a
sreyash <sreyash.s@outlook.com>
2026-02-04 15:12:46 +0530

    implemented stratified group k fold

--- cnn.py
+++ cnn.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 from tensorflow.keras import models, layers, optimizers, callbacks # type: ignore
-from sklearn.model_selection import GroupShuffleSplit
+from sklearn.model_selection import StratifiedGroupKFold
 
 var = int(input("1 => Train\n2 => Test\n\n: "))
 
@@ -16,26 +16,29 @@ def load():
     a = np.load(os.path.join(PATH, 'actors.npy'))
     if x.ndim == 3:
         x = np.expand_dims(x, axis=-1)
-    return x, y, g, a
+    return x, y, a, g
 
 def split(x, y, a, g):
-    actors = np.unique(a)
-    np.random.seed(42)
-    np.random.shuffle(actors)
-    test_actors = actors[:2]
-    val_actors = actors[2:4]
-    train_actors = actors[4:]
-    test_mask = np.isin(a, test_actors)
-    val_mask = np.isin(a, val_actors)
-    train_mask = np.isin(a, train_actors)
-    x_test, y_test, a_test, g_test = x[test_mask], y[test_mask], a[test_mask], g[test_mask]
-    x_val, y_val, a_val, g_val = x[val_mask], y[val_mask], a[val_mask], g[val_mask]
-    x_train, y_train, a_train, g_train = x[train_mask], y[train_mask], a[train_mask], g[train_mask]
-    print("-" * 30)
-    print(f"Train samples: {len(x_train)}")
-    print(f"Val samples:   {len(x_val)}")
-    print(f"Test samples:  {len(x_test)}")
-    print("-" * 30)
+    sgkf_outer = StratifiedGroupKFold(n_splits=10, shuffle=True, random_state=42)
+    train_temp_idx, test_idx = next(sgkf_outer.split(x, y, groups=a))
+    x_test = x[test_idx]
+    y_test = y[test_idx]
+    a_test = a[test_idx]
+    g_test = g[test_idx]
+    x_temp = x[train_temp_idx]
+    y_temp = y[train_temp_idx]
+    a_temp = a[train_temp_idx]
+    g_temp = g[train_temp_idx]
+    sgkf_inner = StratifiedGroupKFold(n_splits=9, shuffle=True, random_state=42)
+    train_idx, val_idx = next(sgkf_inner.split(x_temp, y_temp, groups=a_temp))
+    x_train = x_temp[train_idx]
+    y_train = y_temp[train_idx]
+    a_train = a_temp[train_idx]
+    g_train = g_temp[train_idx]
+    x_val = x_temp[val_idx]
+    y_val = y_temp[val_idx]
+    a_val = a_temp[val_idx]
+    g_val = g_temp[val_idx]
     return (x_train, x_test, x_val), (y_train, y_test, y_val), (a_train, a_test, a_val), (g_train, g_test, g_val)
 
 def init(shape):
