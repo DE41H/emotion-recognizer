dff7d6e8fe26e58ddde6eaea4c90a4252fcb5722
sreyash <sreyash.s@outlook.com>
2026-02-18 20:39:33 +0530

    increased weight decay

--- cnn.py
+++ cnn.py
@@ -59,12 +59,12 @@ def init(shape):
     gmp = layers.GlobalMaxPooling2D()(x)
     x = layers.Concatenate()([gap, gmp])
 
-    x = layers.Dense(128, use_bias=False, kernel_regularizer=regularizers.l2(0.01))(x)
+    x = layers.Dense(128, use_bias=False, kernel_regularizer=regularizers.l2(0.05))(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('elu')(x)
     x = layers.Dropout(0.5)(x)
 
-    x = layers.Dense(64, use_bias=False, kernel_regularizer=regularizers.l2(0.01))(x)
+    x = layers.Dense(64, use_bias=False, kernel_regularizer=regularizers.l2(0.05))(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('elu')(x)
     x = layers.Dropout(0.5)(x)
